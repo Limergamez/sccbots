9db7be2c642ebf7982e093487d15e25395aee815
IzakScherer <125164148+IzakScherer@users.noreply.github.com>
2024-11-12 11:50:13 +1030

    Swapped Servo buttons

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shared/common/ServoControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shared/common/ServoControl.java
@@ -16,9 +16,9 @@ public class ServoControl extends OpMode {
     @Override
     public void loop() {
         // The X button should move the servo forward while the Square reverses
-        if (gamepad1.a) {
+        if (gamepad1.b) {
             myServo.setPosition(1.0); // Full forward position
-        } else if (gamepad1.b) {
+        } else if (gamepad1.a) {
             myServo.setPosition(0.0); // Full reverse position
         } //else {
             //myServo.setPosition(0.5); // iNeutral positionI dont think this is required
