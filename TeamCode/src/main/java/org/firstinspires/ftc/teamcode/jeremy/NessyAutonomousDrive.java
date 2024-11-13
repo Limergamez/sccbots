d4617bd188802f78669cec1d8b4e4606ff882a48
Tyler <tylerkinross0@gmail.com>
2024-11-13 14:57:14 +1030

    Autonomous update. Added the ability to control claw, arm, and lift. It is easier to set commands simply by typing it in so autonomous can be tinkered with further.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jeremy/NessyAutonomousDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jeremy/NessyAutonomousDrive.java
@@ -4,6 +4,8 @@ import static java.lang.Thread.sleep;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.teamcode.shared.common.ArmControl;
 
 @com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "NessyAutonomous")
 public class NessyAutonomousDrive extends LinearOpMode {
@@ -13,64 +15,98 @@ public class NessyAutonomousDrive extends LinearOpMode {
     protected DcMotor centreLeft;
     protected DcMotor backLeft;
 
+    // Claw Servo, Lift Motor, and Arm Motor
+    protected Servo clawServo;
+    protected DcMotor liftMotor;
+    protected DcMotor armMotor;
+    protected ArmControl armControl;
+
     @Override
     public void runOpMode() throws InterruptedException {
-        // Movement motors
+        // Initialize motors
         centreRight = hardwareMap.get(DcMotor.class, "centreRight");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
         centreLeft = hardwareMap.get(DcMotor.class, "centreLeft");
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
 
-        // Setup motor directions
+        clawServo = hardwareMap.get(Servo.class, "clawServo");
+        liftMotor = hardwareMap.get(DcMotor.class, "liftMotor");
+        armMotor = hardwareMap.get(DcMotor.class, "armMotor");
+
+        // Set motor directions
         centreRight.setDirection(DcMotorSimple.Direction.REVERSE);
         backRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        armControl = new ArmControl(null, null, armMotor);
+
         waitForStart();
 
-        // Autonomous Code
-        moveForward(1, 400);
+        // Autonomous sequence
+        sleep(1000);
+        closeClaw();
         sleep(500);
-        moveForward(1, 100);
-        sleep(500);
-        moveLeft(1, 400);
-        sleep(500);
-        moveRight(1, 400);
+        moveForward(-0.30, 2000);
+        raiseLift(1.0, 2000);
+        openClaw();
+        sleep(3000);
+        lowerLift(0.5, 2000);
+        moveArmDown(0.37, 1000);
     }
 
-    public void moveForward(int power, int time) throws InterruptedException {
+    public void moveForward(double power, int time) throws InterruptedException {
         centreRight.setPower(power);
         centreLeft.setPower(power);
         backRight.setPower(power);
         backLeft.setPower(power);
-
-        // Move forward for the set time
         sleep(time);
-
-        // Stop the motors
         stopMotors();
     }
 
-    public void moveLeft(int power, int time) throws InterruptedException {
-        // Setting -Power can help steer each motor
+    public void moveLeft(double power, int time) throws InterruptedException {
         centreRight.setPower(-power);
         backRight.setPower(-power);
         centreLeft.setPower(power);
         backLeft.setPower(power);
-
         sleep(time);
-
         stopMotors();
     }
 
-    public void moveRight(int power, int time) throws InterruptedException {
+    public void moveRight(double power, int time) throws InterruptedException {
         centreRight.setPower(power);
         backRight.setPower(power);
         centreLeft.setPower(-power);
         backLeft.setPower(-power);
-
         sleep(time);
-
         stopMotors();
+    }
+
+    public void openClaw() {
+        clawServo.setPosition(1.0);
+    }
+    public void closeClaw() {
+        clawServo.setPosition(0.0);
+    }
+
+    public void raiseLift(double power, int time) throws InterruptedException {
+        liftMotor.setPower(-power);
+        sleep(time);
+        liftMotor.setPower(0);
+    }
+    public void lowerLift(double power, int time) throws InterruptedException {
+        liftMotor.setPower(power);
+        sleep(time);
+        liftMotor.setPower(0);
+    }
+
+    public void moveArmUp(double power, int time) throws InterruptedException {
+        armMotor.setPower(power);
+        sleep(time);
+        armMotor.setPower(0);
+    }
+    public void moveArmDown(double power, int time) throws InterruptedException {
+        armMotor.setPower(-power);
+        sleep(time);
+        armMotor.setPower(0);
     }
 
     public void stopMotors() {
