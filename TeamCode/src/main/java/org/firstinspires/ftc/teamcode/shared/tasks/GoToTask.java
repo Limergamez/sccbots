1d8e9a813fac892800d5caa833accf051075e292
Tyler <tylerkinross0@gmail.com>
2024-11-28 17:07:17 +1030

    Code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shared/tasks/GoToTask.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shared/tasks/GoToTask.java
@@ -5,8 +5,23 @@ import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 import org.firstinspires.ftc.teamcode.tessy.NormalisedMecanumDrive;
 
 public class GoToTask implements Task {
-    public GoToTask(NormalisedMecanumDrive drive, SparkFunOTOS odometry, int i, int i1, double v) {
+    private final NormalisedMecanumDrive drive;
+    private final SparkFunOTOS odometry;
+    private final double targetX;
+    private final double targetY;
+    private final double error;
+    private double posX;
+    private double posY;
+    private double distance;
 
+    public GoToTask(NormalisedMecanumDrive drive, SparkFunOTOS odometry, double targetX, double targetY, double speed) {
+        this.drive = drive;
+        this.odometry = odometry;
+        this.targetX = targetX;
+        this.targetY = targetY;
+        this.error = speed;
+        this.posX = odometry.getPosition().x;
+        this.posY = odometry.getPosition().y;
     }
 
     @Override
@@ -16,11 +31,15 @@ public class GoToTask implements Task {
 
     @Override
     public void run() {
-
+        double deltaX = targetX - posX;
+        double deltaY = targetY - posY;
+        double distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+        double angle = Math.atan2(deltaY, deltaX);
+        drive.setSpeedPolarR(0.75, angle, 0);
     }
 
     @Override
     public boolean isFinished() {
-        return false;
+        return{distane <= error};
     }
 }
