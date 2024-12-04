3ed5d489ff15d4f39ef01587eabca7c3d687cef9
IzakScherer <125164148+IzakScherer@users.noreply.github.com>
2024-12-04 14:31:26 +1030

    Merge remote-tracking branch 'origin/master'

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shared/tasks/GoToTask.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shared/tasks/GoToTask.java
@@ -5,22 +5,8 @@ import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 import org.firstinspires.ftc.teamcode.tessy.NormalisedMecanumDrive;
 
 public class GoToTask implements Task {
-    private final NormalisedMecanumDrive drive;
-    private final SparkFunOTOS odometry;
-    private final double targetX;
-    private final double targetY;
-    private final double speed;
-    private double posX;
-    private double posY;
+    public GoToTask(NormalisedMecanumDrive drive, SparkFunOTOS odometry, int i, int i1, double v) {
 
-    public GoToTask(NormalisedMecanumDrive drive, SparkFunOTOS odometry, double targetX, double targetY, double speed) {
-        this.drive = drive;
-        this.odometry = odometry;
-        this.targetX = targetX;
-        this.targetY = targetY;
-        this.speed = speed;
-        this.posX = odometry.getPosition().x;
-        this.posY = odometry.getPosition().y;
     }
 
     @Override
@@ -30,9 +16,6 @@ public class GoToTask implements Task {
 
     @Override
     public void run() {
-        double deltaX = targetX - posX;
-        double deltaY = targetY - posY;
-        double distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
 
     }
 
